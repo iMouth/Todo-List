9bdde575f69a24182408b1a2b883985e39b8846f
Kelvin Etori <kelvin@email.arizona.edu>
2022-05-22 00:39:15 -0700

    js for task

--- src/index.js
+++ src/index.js
@@ -56,8 +56,64 @@ function makeExample() {
   curProject = projectBtn;
 }
 
+function changeTaskView() {
+  let taskModal = document.getElementById("form-box");
+  if (taskModal.style.display == "flex") {
+    taskModal.style.display = "none";
+  } else {
+    taskModal.style.display = "flex";
+  }
+}
+
+function addTask() {
+  const todo = document.getElementById("todo");
+
+  const task = document.createElement("div");
+  task.classList.add("task");
+
+  const priority = document.createElement("div");
+  priority.classList.add("priority");
+  task.appendChild(priority);
+
+  const name = document.createElement("p");
+  name.classList.add("name");
+  name.textContent = "Go to work";
+  task.appendChild(name);
+
+  const details = document.createElement("button");
+  details.classList.add("details");
+  details.textContent = "Details";
+  task.appendChild(details);
+
+  const date = document.createElement("p");
+  date.classList.add("date");
+  date.textContent = "July 19th";
+  task.appendChild(date);
+
+  const edit = document.createElement("button");
+  edit.classList.add("edit");
+  const editIcon = document.createElement("img");
+  editIcon.classList.add("edit-icon");
+  editIcon.setAttribute("src", "../dist/img/edit.png");
+  edit.appendChild(editIcon);
+  task.appendChild(edit);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete");
+  const deleteIcon = document.createElement("img");
+  deleteIcon.classList.add("delete-icon");
+  deleteIcon.setAttribute("src", "../dist/img/trash.png");
+  deleteBtn.appendChild(deleteIcon);
+  task.appendChild(deleteBtn);
+
+  todo.appendChild(task);
+}
+
 let curProject = null;
 makeExample();
+addTask();
 document.getElementById("add").addEventListener("click", addProject);
 document.getElementById("cancel").addEventListener("click", changeView);
 document.getElementById("add-project").addEventListener("click", changeView);
+document.getElementById("add-task").addEventListener("click", changeTaskView);
+document.getElementById("cancel-btn").addEventListener("click", changeTaskView);
