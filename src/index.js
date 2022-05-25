4fcea5b5858d6194d72d5b3ca6fcaac624453b12
Kelvin Etori <kelvin@email.arizona.edu>
2022-05-25 10:18:37 -0700

    edit modal

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./styles/normalize.css";
 import "./styles/task.css";
 import "./styles/details.css";
 import "./styles/todo.css";
+import "./styles/edit.css";
 
 function addProject() {
   let projects = document.getElementById("projects");
@@ -11,6 +12,7 @@ function addProject() {
   if (projectName == "") {
     return;
   }
+
   let projectBtn = document.createElement("button");
   projectBtn.setAttribute("id", "project");
   projectBtn.textContent = projectName;
@@ -51,12 +53,12 @@ function changeProjectView() {
 }
 
 function changeTaskView() {
-  // @todo Clear form when cancel is clicked
   if (curProject == null) {
     return;
   }
   changeOpacitiy();
   changeDisplay(document.getElementById("form-box"));
+  document.getElementById("add-form").reset();
 }
 
 function addTask(nameText, dateText, priorityChoice) {
@@ -68,17 +70,7 @@ function addTask(nameText, dateText, priorityChoice) {
 
   const priority = document.createElement("div");
   priority.classList.add("priority");
-  switch (priorityChoice) {
-    case "Low":
-      priority.style.backgroundColor = "lime";
-      break;
-    case "Medium":
-      priority.style.backgroundColor = "goldenrod";
-      break;
-    case "High":
-      priority.style.backgroundColor = "crimson";
-      break;
-  }
+  changePriorityColor(priorityChoice, priority);
   task.appendChild(priority);
 
   const name = document.createElement("p");
@@ -106,6 +98,7 @@ function addTask(nameText, dateText, priorityChoice) {
 
   const edit = document.createElement("button");
   edit.classList.add("edit");
+  edit.addEventListener("click", setEdit);
   const editIcon = document.createElement("img");
   editIcon.classList.add("edit-icon");
   editIcon.setAttribute("src", "../dist/img/edit.png");
@@ -158,12 +151,8 @@ function setUpTasks() {
 }
 
 function deleteTask(e) {
-  console.log(e);
-  console.log(todoDict);
   let name = e.path[2].childNodes[1].innerText;
-  console.log(name);
   delete todoDict[curProject][name];
-  console.log(todoDict);
   e.path[2].remove();
 }
 
@@ -182,9 +171,20 @@ function setDetails(e) {
     "Description: " + info.desc;
 }
 
+function setEdit(e) {
+  let name = e.path[2].childNodes[1].textContent;
+  let info = todoDict[curProject][name];
+  document.getElementById("edit-name").value = info.name;
+  document.getElementById("edit-desc").value = info.desc;
+  document.getElementById("edit-date").value = info.originalDate;
+  document.getElementById("priority-list-edit").value = info.priority;
+  currentEdit = e.path[2].childNodes;
+  changeEditView();
+}
+
 function taskClick(e) {
   e.preventDefault();
-  changeTaskView();
+
   const taskName = document.getElementById("task-name").value;
   const taskDesc = document.getElementById("task-desc").value;
   const taskDate = document.getElementById("task-date").value;
@@ -201,10 +201,62 @@ function taskClick(e) {
     date: dateFormat,
     priority: taskPriority,
     desc: taskDesc,
+    originalDate: taskDate,
   };
   todoDict[projectName][taskName] = info;
+
+  changeTaskView();
 }
 
+function changeEditView() {
+  changeOpacitiy();
+  changeDisplay(document.getElementById("edit-box"));
+}
+
+function editClick(e) {
+  e.preventDefault();
+
+  const newName = document.getElementById("edit-name").value;
+  const newDesc = document.getElementById("edit-desc").value;
+  const newPriority = document.getElementById("priority-list-edit").value;
+  const newDate = document.getElementById("edit-date").value;
+  const date = newDate.split("-");
+  const dateFormat = format(new Date(date[0], date[1], date[2]), "MMM do");
+
+  const name = currentEdit[1].innerText;
+  const info = todoDict[curProject][name];
+  info.name = newName;
+  info.date = dateFormat;
+  info.desc = newDesc;
+  info.priority = newPriority;
+  info.originalDate = newDate;
+
+  if (name != newName) {
+    todoDict[curProject][newName] = info;
+    delete todoDict[curProject][name];
+  }
+
+  currentEdit[1].textContent = info.name;
+  currentEdit[2].textContent = info.date;
+  changePriorityColor(info.priority, currentEdit[0]);
+  changeEditView();
+}
+
+function changePriorityColor(priority, element) {
+  switch (priority) {
+    case "Low":
+      element.style.backgroundColor = "lime";
+      break;
+    case "Medium":
+      element.style.backgroundColor = "goldenrod";
+      break;
+    case "High":
+      element.style.backgroundColor = "crimson";
+      break;
+  }
+}
+
+let currentEdit = null;
 let curProject = null;
 let todoDict = {};
 document.getElementById("add").addEventListener("click", addProject);
@@ -218,3 +270,7 @@ document.getElementById("add-form").addEventListener("submit", taskClick);
 document
   .getElementById("details-exit")
   .addEventListener("click", changeDetailsView);
+document
+  .getElementById("cancel-btn-edit")
+  .addEventListener("click", changeEditView);
+document.getElementById("edit-form").addEventListener("submit", editClick);
